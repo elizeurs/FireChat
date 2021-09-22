1752e1a82c1e7d6bfcdfcce6c7bc1f2118c7b226
elizeurs <13ders@gmail.com>
2021-09-22 16:40:38 -0300

    Setup Home Screen Navigation Bar

--- FireChat/Controller/ConversationsController.swift
+++ FireChat/Controller/ConversationsController.swift
@@ -16,6 +16,7 @@ class ConversationsController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     configure()
+    configureNavigationBar()
   }
   
   // MARK: - Selectors
@@ -34,5 +35,23 @@ class ConversationsController: UIViewController {
     
     let image = UIImage(systemName: "person.circle.fill")
     navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style: .plain, target: self, action: #selector(showProfile))
+  }
+  
+  func configureNavigationBar() {
+    let appearance = UINavigationBarAppearance()
+    appearance.configureWithOpaqueBackground()
+    appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+    appearance.backgroundColor = .purple
+
+    navigationController?.navigationBar.standardAppearance = appearance
+    navigationController?.navigationBar.compactAppearance = appearance
+    navigationController?.navigationBar.scrollEdgeAppearance = appearance
+
+    navigationController?.navigationBar.prefersLargeTitles = true
+    navigationItem.title = "Messages"
+    navigationController?.navigationBar.tintColor = .white
+    navigationController?.navigationBar.isTranslucent = true
+
+    navigationController?.navigationBar.overrideUserInterfaceStyle = .dark
   }
 }
