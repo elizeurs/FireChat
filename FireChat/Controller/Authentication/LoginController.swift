69afe34e5caa102b6ec4d94b5b9669268714db98
elizeurs <13ders@gmail.com>
2021-09-27 19:45:55 -0300

    API Code Refactor

--- FireChat/Controller/Authentication/LoginController.swift
+++ FireChat/Controller/Authentication/LoginController.swift
@@ -87,18 +87,14 @@ class LoginController: UIViewController {
   @objc func handleLogin() {
     guard let email = emailTextField.text else { return }
     guard let password = passwordTextField.text else { return }
-    
-    Auth.auth().signIn(withEmail: email, password: password) { result, error in
+    AuthService.shared.logUserIn(withEmail: email, password: password) { result, error in
       if let error = error {
-        print("DEBUG: Failed to log in with error \(error.localizedDescription)")
+        print("DEBUG: Failed to login with error \(error.localizedDescription)")
         return
       }
       
       self.dismiss(animated: true, completion: nil)
-//      print("DEBUG: User login successful")
     }
-    
-//    print("DEBUG: Handle login here..")
   }
   
   @objc func handleShowSignUp() {
--- FireChat/Controller/Authentication/RegistrationController.swift
+++ FireChat/Controller/Authentication/RegistrationController.swift
@@ -102,52 +102,17 @@ class RegistrationController: UIViewController {
     guard let fullname = fullnameTextField.text else { return }
     guard let username = usernameTextField.text?.lowercased() else { return }
     guard let profileImage = profileImage else { return }
+
+    let credentials = RegistrationCredentials(email: email, password: password, fullname: fullname, username: username, profileImage: profileImage)
     
-    guard let imageData = profileImage.jpegData(compressionQuality: 0.3) else { return }
-    
-    let filename = NSUUID().uuidString
-    let ref = Storage.storage().reference(withPath: "/profile_image/\(filename)")
-    
-    ref.putData(imageData, metadata: nil) { (Meta, error) in
+    AuthService.shared.createUser(credentials: credentials) { error in
       if let error = error {
-        print("DEBUG: Failed to upload image with error \(error.localizedDescription)")
+        print("DEBUG: \(error.localizedDescription)")
         return
       }
       
-      ref.downloadURL { (url, error) in
-        guard let profileImageUrl = url?.absoluteString else { return }
-        
-        Auth.auth().createUser(withEmail: email, password: password) { (result, error) in
-          if let error = error {
-            print("DEBUG: Failed to create user with error: \(error.localizedDescription)")
-            return
-          }
-          
-          guard let uid = result?.user.uid else { return }
-          
-          let data = ["email": email,
-                      "fullname": fullname,
-                      "profileImageUrl": profileImageUrl,
-                      "uid": uid,
-                      "username": username] as [String : Any]
-          
-          Firestore.firestore().collection("users").document(uid).setData(data) { error in
-            if let error = error {
-              print("DEBUG: Failed to upload user data with error: \(error.localizedDescription)")
-              return
-            }
-            
-            self.dismiss(animated: true, completion: nil)
-//            print("DEBUG: Did create user..")
-          }
-        }
-      }
+      self.dismiss(animated: true, completion: nil)
     }
-
-    
-//    print(email)
-//    print(password)
-//    print(fullname)
   }
   
   
